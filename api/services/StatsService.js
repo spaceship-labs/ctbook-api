1f8a47856669aac839c6a55d05127848adad71ba
el-sonny <sonny@spaceshiplabs.com>
2015-11-18 17:28:44 -0600

    expanded stats

--- api/services/StatsService.js
+++ api/services/StatsService.js
@@ -12,19 +12,36 @@ module.exports = {
     collection = gauss.Collection;
     var _contracts = new collection(contracts);
     var limits = getMinMaxYears(_contracts);
-    var years = [];
-    var ammounts = [];
-    for (var i = limits.min; i <= limits.max; i++) {
-      years.push(i);
-      ammounts.push(getTotalForYear(i, _contracts));
+    var series = [];
+    var types = getProcedureTypes(_contracts);
+
+    for (var j = 0; j < types.length; j++) {
+      var values = [];
+      for (var i = limits.min; i <= limits.max; i++) {
+        values.push({
+          year: i,
+          ammount: getSeries(_contracts, {
+            fecha_inicio_year: i,
+            tipo_procedimiento: types[j]
+          })
+        });
+      }
+      series.push({
+        key: types[j],
+        values: values
+      });
     }
-    return {
-      years: years,
-      ammounts: ammounts
-    };
+    return series;
 
   }
 }
+
+var getProcedureTypes = function(contracts) {
+  return contracts.map(function(contract) {
+    return contract.tipo_procedimiento
+  }).unique();
+}
+
 var getMinMaxYears = function(contracts) {
   var vector =
     contracts.map(function(contract) {
@@ -36,10 +53,8 @@ var getMinMaxYears = function(contracts) {
   };
 }
 
-var getTotalForYear = function(year, contracts) {
-  return contracts.find({
-    fecha_inicio_year: year
-  }).map(function(contract) {
+var getSeries = function(contracts, params) {
+  return contracts.find(params).map(function(contract) {
     return contract.importe_contrato
   }).toVector().sum();
 }
